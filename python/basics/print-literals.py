a3ce9be852632b466fd8af8937a88ba5f4b88090
James Shaw <jshaw@users.noreply.github.com>
2026-08-03 21:22:49 +0100

    Add string literal examples and comments to print-literals.py

--- python/basics/print-literals.py
+++ python/basics/print-literals.py
@@ -6,3 +6,11 @@ print(0o123) # This is  is an octal number with a (decimal) value equal to 83.
 print(0x123) # This is a hexadecimal number with a (decimal) value equal to 291.
 
 print(0.0000000000000000000001) # This is a floating point number with a (decimal) value equal to 1e-22.
+
+print("I like \"Steak\"") # This is a string literal with an escaped double quote character.
+
+print('I like "Steak"') # This is a string literal with a double quote character.
+
+# If you open a string with a quote, you have to close it with a quote.
+
+# If you start a string with an apostrophe, you have to end it with an apostrophe.
