5ebd051ef9a96031b66b781efe0c45df178fb569
James Shaw <jshaw@users.noreply.github.com>
2026-08-03 21:16:44 +0100

    Add examples of octal and hexadecimal number literals

--- python/basics/print-literals.py
+++ python/basics/print-literals.py
@@ -1,2 +1,6 @@
 print("2") # this is a string literal, not a number
 print(2) # this is a number literal, not a string
+
+print(0o123) # This is  is an octal number with a (decimal) value equal to 83.
+
+print(0x123) # This is a hexadecimal number with a (decimal) value equal to 291.
